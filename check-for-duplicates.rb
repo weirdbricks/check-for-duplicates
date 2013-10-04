3018770a839822fb0145ff01b213ccad92d3483c
root <root@weirdbricks.(none)>
2013-10-04 00:51:01 +0000

    first commit

---
+++ check-for-duplicates.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+#by Lampros Chaidas for WeirdBricks
+#checks for duplicate files under /uploads
+#if duplicates are found, ignores and e-mails
+#if all well still e-mails
+
+
+
