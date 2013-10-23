cb583aec89e9993ec598251c26ed5b7e2d58bd72
weirdbricks <weirdbricks@weirdbricks.(none)>
2013-10-22 22:04:26 -0700

    hash check works

--- check-for-duplicates.rb
+++ check-for-duplicates.rb
@@ -1,20 +1,41 @@
 #!/usr/bin/env ruby
 
 #by Lampros Chaidas for WeirdBricks
-#checks for duplicate files under /uploads
+#checks for duplicate files under /unprocessed-photos
 #if duplicates are found, ignores and e-mails
 #if all well still e-mails
 
-require 'date'
+#gem install sequel --no-ri --no-rdoc
+#yum install sqlite-devel
+#gem install sqlite3 --no-ri --no-rdoc
 
+require 'date'
+require 'digest'
+require 'sequel'
+require 'sqlite3'
+
+sqlite3="/home/weirdbricks/processed-photos.db"
 source_directory="/home/weirdbricks/unprocessed-photos/*"
 target_directory="/home/weirdbricks/archived-photos"
 
-#a function that logs directory to a file
+
+DB = Sequel.sqlite(sqlite3)
+
+#create the tables only if the file doesn't exist!
+unless File.exists?(sqlite3)
+  DB.create_table :photos do
+    primary_key :id
+    String :filename
+    Integer :size
+    String :hash
+   end
+end
+
+#a function that logs messages to a file
 def log(text)
    puts text
    logfile="/home/weirdbricks/duplicates"+DateTime.now.strftime("%H-%M_%m-%d-%Y")+".log"
-   `echo #{text} >> #{logfile}`
+   `echo "#{text}" >> #{logfile}`
 end
 
 def remove_spaces_from_directories(directories)
@@ -38,4 +59,27 @@ end
 directories = `find #{source_directory} -type d`.split("\n")
 log "Checking directories for spaces.."
 remove_spaces_from_directories directories
+
+dataset = DB[:photos]
+
+#go through each directory and process files
+directories.each do |directory|
+  files=`find /home/weirdbricks/unprocessed-photos/* -type f`.split("\n")
+  #go through each file calculate it's hash and check if it exists in the database
+  files.each do |file|
+    hash = Digest::SHA1.hexdigest File.read(file)
+    size = File.size(file)
+    result = dataset.where(:hash=>hash)
+    if result.count==0
+    log "INFO: file #{file} is new - adding to the database - HASH: #{hash}, SIZE: #{size}"
+      dataset.insert(:filename => file,
+                     :size => size,
+                     :hash => hash)
+    else
+      log "WARNING: file: #{file} already exists! skipping!"
+    end
+
+  end
+
+end
 
