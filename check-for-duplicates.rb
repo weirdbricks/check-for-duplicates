06a2f8c245defc139edb6b4da224c036ce0fe957
weirdbricks <weirdbricks@weirdbricks.(none)>
2013-10-22 20:50:46 -0700

    added diretory checks

--- check-for-duplicates.rb
+++ check-for-duplicates.rb
@@ -5,5 +5,32 @@
 #if duplicates are found, ignores and e-mails
 #if all well still e-mails
 
+require 'date'
+
+source_directory="/home/weirdbricks/unprocessed-photos/*"
+target_directory="/home/weirdbricks/archived-photos"
+
+#a function that logs directory to a file
+def log(text)
+   logfile="/home/weirdbricks/duplicates"+DateTime.now.strftime("%H-%M_%m-%d-%Y")+".log"
+   `echo #{text} >> #{logfile}`
+end
+
+#get directories and split them on \n, which makes them an array
+directories = `find #{source_directory} -type d`.split("\n")
+directories.each do |item|
+  #we don't want to keep spaces, so find them and rename them
+  if item.include?(" ") 
+    log "Directory #{item} has a space!"
+    newitem=item.gsub(" ","_")
+    puts newitem
+    #renaming (moving) directory
+    log "Renaming directory: #{item}"
+    `mv "#{item}" #{newitem}`
+    if Dir.exists?(newitem)
+      log "Renaming of directory: #{item} SUCCESS"
+    end
+  end
+end
 
 
