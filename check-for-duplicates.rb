acc4b147677c6b9cc05b4ad22b00d78a8f78396f
weirdbricks <weirdbricks@weirdbricks.(none)>
2013-10-22 20:55:57 -0700

    converted into function

--- check-for-duplicates.rb
+++ check-for-duplicates.rb
@@ -12,25 +12,30 @@ target_directory="/home/weirdbricks/archived-photos"
 
 #a function that logs directory to a file
 def log(text)
+   puts text
    logfile="/home/weirdbricks/duplicates"+DateTime.now.strftime("%H-%M_%m-%d-%Y")+".log"
    `echo #{text} >> #{logfile}`
 end
 
-#get directories and split them on \n, which makes them an array
-directories = `find #{source_directory} -type d`.split("\n")
-directories.each do |item|
-  #we don't want to keep spaces, so find them and rename them
-  if item.include?(" ") 
-    log "Directory #{item} has a space!"
-    newitem=item.gsub(" ","_")
-    puts newitem
-    #renaming (moving) directory
-    log "Renaming directory: #{item}"
-    `mv "#{item}" #{newitem}`
-    if Dir.exists?(newitem)
-      log "Renaming of directory: #{item} SUCCESS"
+def remove_spaces_from_directories(directories)
+  directories.each do |item|
+    #we don't want to keep spaces, so find them and rename them
+    if item.include?(" ")
+      log "Directory #{item} has a space!"
+      newitem=item.gsub(" ","_")
+      puts newitem
+      #renaming (moving) directory
+      log "Renaming directory: #{item}"
+     `mv "#{item}" #{newitem}`
+       if Dir.exists?(newitem)
+         log "Renaming of directory: #{item} SUCCESS"
+       end
     end
   end
 end
 
+#get directories and split them on \n, which makes them an array
+directories = `find #{source_directory} -type d`.split("\n")
+log "Checking directories for spaces.."
+remove_spaces_from_directories directories
 
